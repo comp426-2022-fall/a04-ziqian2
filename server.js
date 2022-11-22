3c791a43fce753a442ec0017932454d2ef3f2dda
ziqian2 <77801249+ziqian2@users.noreply.github.com>
2022-11-21 21:31:30 -0500

    b

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/app/', (req, res) => {
 
 // requirement #4
 app.get('/app/roll/', (req, res) => {
-    res.send(roll(6,2,1));
+    res.status(200).send(roll(6,2,1));
 });
 
 // requirement #5
@@ -27,13 +27,13 @@ app.post('/app/roll/', (req, res) =>{
     const dice = parseInt(req.body.dice);
     const rolls = parseInt(req.body.rolls);
     
-    res.send(roll(sides, dice, rolls));
+    res.status(200).send(roll(sides, dice, rolls));
 })
 
 // requirement #6
 app.get('/app/roll/:sides/', (req, res) =>{
     const sides = parseInt(req.params.sides);
-    res.send(roll(sides, 2, 1));
+    res.status(200).send(roll(sides, 2, 1));
 })
 
 
@@ -41,7 +41,7 @@ app.get('/app/roll/:sides/', (req, res) =>{
 app.get('/app/roll/:sides/:dice', (req, res) =>{
     const sides = parseInt(req.params.sides);
     const dice = parseInt(req.params.dice);
-    res.send(roll(sides, dice, 1));
+    res.status(200).send(roll(sides, dice, 1));
 })
 
 //requirement #8
@@ -49,7 +49,7 @@ app.get('/app/roll/:sides/:dice/:rolls/', (req, res) =>{
     const sides = parseInt(req.params.sides);
     const dice = parseInt(req.params.dice);
     const rolls = parseInt(req.params.rolls);
-    res.send(roll(sides, dice, rolls));
+    res.status(200).send(roll(sides, dice, rolls));
 })
 
 app.use((req, res) => {
