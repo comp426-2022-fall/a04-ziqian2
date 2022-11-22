9205d4f10c6100d115f54f9398433d2892817029
ziqian2 <77801249+ziqian2@users.noreply.github.com>
2022-11-21 19:24:27 -0500

    try

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+#!/usr/bin/env node
+
 import express from 'express';
 import minimist from 'minimist';
 import { roll } from './lib/roll.js';
+
 
 const args = minimist(process.argv.slice(2));
 const app = express();
@@ -20,38 +23,37 @@ app.get('/app/roll/', (req, res) => {
 
 // requirement #5
 app.post('/app/roll/', (req, res) =>{
-    const sides = parseInt(req.body.sides);
-    const dice = parseInt(req.body.dice);
-    const rolls = parseInt(req.body.rolls);
+    let sides = parseInt(req.body.sides);
+    let dice = parseInt(req.body.dice);
+    let rolls = parseInt(req.body.rolls);
     
     res.send(roll(sides, dice, rolls));
 })
 
 // requirement #6
 app.get('/app/roll/:sides/', (req, res) =>{
-    const sides = parseInt(req.params.sides);
+    let sides = parseInt(req.params.sides);
     res.send(roll(sides, 2, 1));
 })
 
 
 //requirement #7
 app.get('/app/roll/:sides/:dice', (req, res) =>{
-    const sides = parseInt(req.params.sides);
-    const dice = parseInt(req.params.dice);
+    let sides = parseInt(req.params.sides);
+    let dice = parseInt(req.params.dice);
     res.send(roll(sides, dice, 1));
 })
 
 //requirement #8
 app.get('/app/roll/:sides/:dice/:rolls/', (req, res) =>{
-    const sides = parseInt(req.params.sides);
-    const dice = parseInt(req.params.dice);
-    const rolls = parseInt(req.params.rolls);
+    let sides = parseInt(req.params.sides);
+    let dice = parseInt(req.params.dice);
+    let rolls = parseInt(req.params.rolls);
     res.send(roll(sides, dice, rolls));
 })
 
-app.get('*', (req, res)=>{
-    res.status(404).send("404 NOT FOUND");
-})
+app.use((req, res) =>  {
+	res.status(404).send('404 NOT FOUND');
+});
 
 app.listen(port);
-
